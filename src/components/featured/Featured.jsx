9aa9f24c7e68b96015b2e37a6f2a90c224030b45
Eusebio Joveth <eusebio.joveth@porlande.com>
2023-07-12 09:13:30 +0100

    Conclusao da implementacao do navBar

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -6,10 +6,10 @@ function Featured() {
     <div className='featured'>
         <div className="container">
             <div className="left">
-                <h1>Encontre os servicos freelance perfeitos para seu negocio</h1>
+                <h1>Encontre os servicos <i>freelance</i> perfeitos para seu negocio</h1>
                 <div className="search">
                     <div className="searchInput">
-                        <img src="" alt="" />
+                        <img src="./img/search.png" alt="" />
                         <input type="text" placeholder='Tente criar um aplicativo' />
                     </div>
                     <button>Pesquisar</button>
@@ -17,9 +17,14 @@ function Featured() {
                 <div className="popular">
                     <span>Popular:</span>
                     <button>Web Design</button>
+                    <button>Worpress</button>
+                    <button>Design de Logotipo</button>
+                    <button>Serviços AI</button>
                 </div>
             </div>
-            <div className="right"></div>
+            <div className="right">
+                <img src="./img/man.png" alt="" />
+            </div>
         </div>
     </div>
   )
