846ded58274ea8321c7a1dda90aa237254ec04e3
Eusebio Joveth <eusebio.joveth@porlande.com>
2023-07-28 14:45:33 +0100

    footer implementado e conclusao da pgaina home

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,101 @@
 import React from 'react'
 import "./Footer.scss";
 
-function Footer() {
+const Footer = () => {
   return (
-    <div className='footer'>Footer</div>
+    <div className='footer'>
+      <div className="container">
+        <div className="top">
+          <div className="item">
+            <h2>Categorias</h2>
+            <span>Gráficos e Design</span>
+            <span>Marketing Digital</span>
+            <span>Redação e Tradução</span>
+            <span>Vídeo e Animação</span>
+            <span>Música e Audio</span>
+            <span>Programação e Tecnologia</span>
+            <span>Dados</span>
+            <span>Negócios</span>
+            <span>Estilo de Vida</span>
+            <span>Fotografia</span>
+            <span>Mapa do Site</span>
+          </div>
+
+          <div className="item">
+            <h2>Categorias</h2>
+            <span>Gráficos e Design</span>
+            <span>Marketing Digital</span>
+            <span>Redação e Tradução</span>
+            <span>Vídeo e Animação</span>
+            <span>Música e Audio</span>
+            <span>Programação e Tecnologia</span>
+            <span>Dados</span>
+            <span>Negócios</span>
+            <span>Estilo de Vida</span>
+            <span>Fotografia</span>
+            <span>Mapa do Site</span>
+          </div>
+
+          <div className="item">
+            <h2>Categorias</h2>
+            <span>Gráficos e Design</span>
+            <span>Marketing Digital</span>
+            <span>Redação e Tradução</span>
+            <span>Vídeo e Animação</span>
+            <span>Música e Audio</span>
+            <span>Programação e Tecnologia</span>
+            <span>Dados</span>
+            <span>Negócios</span>
+            <span>Estilo de Vida</span>
+            <span>Fotografia</span>
+            <span>Mapa do Site</span>
+          </div>
+
+          <div className="item">
+            <h2>Categorias</h2>
+            <span>Gráficos e Design</span>
+            <span>Marketing Digital</span>
+            <span>Redação e Tradução</span>
+            <span>Vídeo e Animação</span>
+            <span>Música e Audio</span>
+            <span>Programação e Tecnologia</span>
+            <span>Dados</span>
+            <span>Negócios</span>
+            <span>Estilo de Vida</span>
+            <span>Fotografia</span>
+            <span>Mapa do Site</span>
+          </div>
+        </div>
+     
+      <hr />
+      <div className="buttom">
+        <div className="left">
+          <h2>MeRecruta</h2>
+          <span>© JovethTecnologia International Ltd. 2023</span>
+        </div>
+        <div className="right">
+          <div className="social">
+            <img src="img/twitter.png" alt="" />
+            <img src="img/facebook.png" alt="" />
+            <img src="img/linkedin.png" alt="" />
+            <img src="img/pinterest.png" alt="" />
+            <img src="img/instagram.png" alt="" />
+          </div>
+
+          <div className="link">
+          <img src="./img/language.png" alt="" />
+          <span>Português</span>
+        </div>
+        <div className="link">
+          <img src="./img/coin.png" alt="" />
+          <span>USD</span>
+        </div>
+        <img src="./img/accessibility.png" alt="" />
+        </div>
+       
+      </div>
+      </div>
+    </div>
   )
 }
 
